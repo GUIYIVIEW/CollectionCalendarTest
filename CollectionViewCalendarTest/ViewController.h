f684c8c765414cc5cbb3afb0cb1bbcb2638d6c71
xzh <xzh@xzhdemac-mini-3.local>
2015-12-24 12:55:56 +0800

    Initial Commit

---
+++ CollectionViewCalendarTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewCalendarTest
+//
+//  Created by xzh on 15/12/24.
+//  Copyright © 2015年 BY-R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewCalendarTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionViewCalendarTest
+//
+//  Created by xzh on 15/12/24.
+//  Copyright © 2015年 BY-R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
